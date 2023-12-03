d0d7f1bd2769655b86c147d4a47c3a47ecaf44f5
Oleksandr-Berd <alex.berd86@gmail.com>
2023-12-03 15:13:43 +0200

    working on the quiz logic

--- src/pages/QuizPage/QuizPage.tsx
+++ src/pages/QuizPage/QuizPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import * as SC from "./QuizPageStyled";
 
 type T = {
@@ -12,38 +13,64 @@ type Props = {
 };
 
 const QuizPage: React.FC<Props> = ({ chosenTopic }) => {
-  if (!chosenTopic) {
-    return <div></div>;
-  }
+  const [chosenOption, setChosenOption] = useState<string | null>(null);
 
-  const { questions } = chosenTopic;
-
-  let currentQuestion = questions[0];
+  const [currentQuestion, setCurrentQuestion] = useState(
+    chosenTopic?.questions[0]
+  );
 
   const gradient =
-    ((questions.indexOf(currentQuestion) + 1) / questions.length) * 100 + "%";
+    ((chosenTopic!.questions.indexOf(currentQuestion!) + 1) /
+      chosenTopic!.questions.length) *
+      100 +
+    "%";
   const gradientLeft =
     100 -
-    ((questions.indexOf(currentQuestion) + 1) / questions.length) * 100 +
+    ((chosenTopic!.questions.indexOf(currentQuestion!) + 1) /
+      chosenTopic!.questions.length) *
+      100 +
     "%";
+
+  const chooseOption = (evt: any) => {
+    setChosenOption(evt.currentTarget.getAttribute("data-title"));
+  };
+
+  const currentIdx = chosenTopic!.questions.indexOf(currentQuestion!);
+
+  const handleSubmitAnswer = () => {
+    setCurrentQuestion(chosenTopic!.questions[currentIdx + 1]);
+  };
 
   return (
     <SC.CommonCon>
       <SC.TextWrapper>
-        <SC.SmallText>Questions 1 of {questions.length}</SC.SmallText>
-        <SC.Question>{currentQuestion.question}</SC.Question>
+        <SC.SmallText>
+          Questions {currentIdx + 1} of {chosenTopic!.questions.length}
+        </SC.SmallText>
+        <SC.Question>{currentQuestion!.question}</SC.Question>
         <SC.Scale gradient={gradient} gradientLeft={gradientLeft}></SC.Scale>
       </SC.TextWrapper>
       <SC.OptionsList>
-        {currentQuestion.options.map((el, idx) => (
-          <SC.OptionItem key={el}>
+        {currentQuestion!.options.map((el, idx) => (
+          <SC.OptionItem key={el} data-title={el} onClick={chooseOption}>
             <SC.LetterWrapper>
-              {idx === 0 ? "A" : idx === 1 ? "B" : idx === 2 ? "C" : idx === 3 ? "D" : null}
+              {idx === 0
+                ? "A"
+                : idx === 1
+                ? "B"
+                : idx === 2
+                ? "C"
+                : idx === 3
+                ? "D"
+                : null}
             </SC.LetterWrapper>
             <SC.Option>{el}</SC.Option>
           </SC.OptionItem>
         ))}
       </SC.OptionsList>
+      <SC.SubmitButton onClick={handleSubmitAnswer}>
+        Submit Answer
+      </SC.SubmitButton>
     </SC.CommonCon>
   );
 };
--- src/pages/QuizPage/QuizPageStyled.ts
+++ src/pages/QuizPage/QuizPageStyled.ts
@@ -6,42 +6,43 @@ type Style = {
 };
 
 export const CommonCon = styled.div`
-padding-top: 32px;
-padding-left: 24px;
-padding-right: 24px;
-`
+  padding-top: 32px;
+  padding-bottom: 139px;
+  padding-left: 24px;
+  padding-right: 24px;
+`;
 
 export const TextWrapper = styled.div`
-margin-bottom: 40px;
-`
+  margin-bottom: 40px;
+`;
 
 export const SmallText = styled.p`
-margin-bottom: 12px;
+  margin-bottom: 12px;
 
-font-style: italic;
-font-family: "RubicRegular";
-font-size: 14px;
-line-height: 1.5;
+  font-style: italic;
+  font-family: "RubicRegular";
+  font-size: 14px;
+  line-height: 1.5;
 
-color: ${props => props.theme.color.smallText};
-`
+  color: ${(props) => props.theme.color.smallText};
+`;
 
 export const Question = styled.h3`
-margin-bottom: 24px;
+  margin-bottom: 24px;
 
-font-size: 20px;
-line-height: 1.2;
-font-family: "RubicMedium";
+  font-size: 20px;
+  line-height: 1.2;
+  font-family: "RubicMedium";
 
-color: ${props => props.theme.color.text};
-`
+  color: ${(props) => props.theme.color.text};
+`;
 
 export const Scale = styled.div<Style>`
   height: 16px;
 
   background: linear-gradient(
     to left,
-    rgb(59, 77, 102) ${props => props.gradientLeft},
+    rgb(59, 77, 102) ${(props) => props.gradientLeft},
     rgb(167, 41, 245) ${(props) => props.gradient}
   );
 
@@ -49,54 +50,71 @@ export const Scale = styled.div<Style>`
 `;
 
 export const OptionsList = styled.ul`
-padding-left: 0;
+  padding-left: 0;
 
-margin-bottom: 12px;
-`
+  margin-bottom: 12px;
+`;
 
 export const OptionItem = styled.li`
-display: flex;
-flex-direction: row;
-align-items: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
-height: 64px;
+  height: 64px;
 
-padding-top: 12px;
-padding-bottom: 12px;
-padding-left: 12px;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  padding-left: 12px;
 
-background-color: ${props => props.theme.color.itemBg};
+  background-color: ${(props) => props.theme.color.itemBg};
 
-border-radius: 12px;
+  border-radius: 12px;
 
-&:not(:last-child){
+  &:not(:last-child) {
     margin-bottom: 12px;
-}
-`
+  }
+`;
 
 export const Option = styled.p`
-margin-top: 11px;
-margin-left: 16px;
+  margin-top: 11px;
+  margin-left: 16px;
 
-font-size: 18px;
-font-family: "RubicMedium";
+  font-size: 18px;
+  font-family: "RubicMedium";
 
-color: ${props => props.theme.color.text}
-`
+  color: ${(props) => props.theme.color.text};
+`;
 
 export const LetterWrapper = styled.div`
-display: flex;
-align-items: center;
-justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   width: 40px;
   height: 40px;
 
-font-size: 18px;
-font-family: "RubicMedium";
+  font-size: 18px;
+  font-family: "RubicMedium";
 
   color: #626c7f;
   background-color: #f4f6fa;
 
   border-radius: 6px;
+`;
+
+export const SubmitButton = styled.button`
+  width: 100%;
+
+  padding-top: 19px;
+  padding-bottom: 19px;
+
+  text-align: center;
+
+  font-size: 18px;
+  font-family: "RubicMedium";
+
+  color: ${(props) => props.theme.color.text};
+  background-color: #a729f5;
+
+  border-radius: 12px;
 `;
